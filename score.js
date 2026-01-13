949ec7acd8c0d73e234b841b3a0b485f7122cf6a
analyticsifyresearchltd <analyticsifyresearchltd@gmail.com>
2026-01-14 03:12:45 +0600

    score.js

--- score.js
+++ score.js
@@ -1,17 +1,23 @@
 async function getScore(){
- let score = 0;
- let flags = [];
+ let score = 0
+ let flags = []
 
- if(fraudData.mouse < 50){score+=20; flags.push("low_mouse")}
- if(fraudData.keys < 5){score+=15; flags.push("low_keys")}
- if(fraudData.scroll < 1){score+=10; flags.push("no_scroll")}
- if(fraudData.tabs > 0){score+=15; flags.push("tab_switch")}
- if(!fraudData.clicked){score+=25; flags.push("attention_fail")}
- if(fraudData.honesty){score+=25; flags.push("honesty_fail")}
+ if(fraudData.mouse < 40){score+=20;flags.push("low_mouse")}
+ if(fraudData.keys < 10){score+=15;flags.push("low_keys")}
+ if(fraudData.scrolls < 1){score+=10;flags.push("no_scroll")}
+ if(fraudData.tabs > 0){score+=15;flags.push("tab_switch")}
 
- let vpn = await checkVPN();
- if(vpn.bad){score+=30; flags.push("vpn")}
+ let a = window.humanAnswers || {}
 
- return {score, flags}
+ if(a.q2 !== "idea"){score+=20;flags.push("concept_fail")}
+ if(a.q3 === "yes"){score+=30;flags.push("fake_location")}
+ if(a.q4 === "defnot"){score+=30;flags.push("logic_fail")}
+
+ if(!a.q5 || a.q5.split(" ").length < 8){score+=25;flags.push("short_text")}
+
+ let vpn = await checkVPN()
+ if(vpn.bad){score+=30;flags.push("vpn")}
+
+ return {score,flags}
 }
-window.getScore = getScore;
+window.getScore = getScore
