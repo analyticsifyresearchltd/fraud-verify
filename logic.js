2d0447ff47e2f8e56d76f111f59d61eee0382c95
analyticsifyresearchltd <analyticsifyresearchltd@gmail.com>
2026-01-18 07:51:48 +0600

    logic.js

--- logic.js
+++ logic.js
@@ -1,32 +1,9 @@
-let currentPage = 0;
-const pages = document.querySelectorAll(".question-page");
+function ruleScore(a){
+ let score = 0
 
-window.answers = { q2:"", q3:"", q4:"", q5:"" };
+ if(a.q2 === "idea") score += 30
+ if(a.q3 === "no") score += 30
+ if(a.q4 === "yes") score += 20
 
-function showPage(i) {
-  pages.forEach(p => p.style.display = "none");
-  pages[i].style.display = "block";
+ return score
 }
-
-showPage(0);
-
-document.addEventListener("click", function(e){
-  if (!e.target.matches("#nextBtn")) return;
-  if (currentPage < pages.length - 1) {
-    currentPage++;
-    showPage(currentPage);
-  }
-});
-
-function selectQ2(v){ answers.q2 = v; next(); }
-function selectQ3(v){ answers.q3 = v; next(); }
-function selectQ4(v){ answers.q4 = v; next(); }
-
-function next(){
-  currentPage++;
-  showPage(currentPage);
-}
-
-document.getElementById("q5text").addEventListener("input", e=>{
-  answers.q5 = e.target.value;
-});
