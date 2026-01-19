467329774b5c6b75a55bdba54dfc8c6dcc146e1b
analyticsifyresearchltd <analyticsifyresearchltd@gmail.com>
2026-01-20 02:14:01 +0600

    vpn.js

--- vpn.js
+++ vpn.js
@@ -1,12 +1,8 @@
-window.vpnScore = 0;
+window.vpnRisk = 0;
 
-fetch("https://ipinfo.io/json")
-  .then(r => r.json())
-  .then(d => {
-    const org = (d.org || "").toLowerCase();
-
-    if (org.includes("vpn")) vpnScore += 40;
-    if (org.includes("hosting") || org.includes("colo")) vpnScore += 30;
-    if (org.includes("google") || org.includes("amazon") || org.includes("azure")) vpnScore += 30;
-  })
-  .catch(() => vpnScore += 20);
+fetch("https://ipapi.co/json/")
+.then(r=>r.json())
+.then(d=>{
+  if(d.proxy || d.hosting) vpnRisk = 50;
+  if(["AS9009","AS14061"].includes(d.asn)) vpnRisk += 30;
+});
