37893df794f3a6eab26172f42a05f8337d065920
analyticsifyresearchltd <analyticsifyresearchltd@gmail.com>
2026-01-20 02:13:32 +0600

    fraud.js

--- fraud.js
+++ fraud.js
@@ -1,16 +1,5 @@
-window.fraud = {
-  mouseMoves: 0,
-  clicks: 0,
-  keys: 0,
-  scrolls: 0,
-  focusLoss: 0,
-  pasteEvents: 0,
-  start: Date.now()
-};
+window.behavior = { clicks:0, keys:0, scroll:0, start:Date.now() };
 
-document.addEventListener("mousemove", () => fraud.mouseMoves++);
-document.addEventListener("click", () => fraud.clicks++);
-document.addEventListener("keydown", () => fraud.keys++);
-document.addEventListener("scroll", () => fraud.scrolls++);
-window.addEventListener("blur", () => fraud.focusLoss++);
-document.addEventListener("paste", () => fraud.pasteEvents++);
+document.addEventListener("click",()=>behavior.clicks++);
+document.addEventListener("keydown",()=>behavior.keys++);
+document.addEventListener("scroll",()=>behavior.scroll++);
